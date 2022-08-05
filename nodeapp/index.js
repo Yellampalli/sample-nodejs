5e577f4cecc6e0dc45bf8c57fff1fc26af4f3c9c
Yellampalli <107525921+Yellampalli@users.noreply.github.com>
2022-08-05 11:17:26 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome Khaja" }');
+    res.send('{ "response": "Hello Kaja anna, today i completed 3 tasks " }');
 });
 
 app.get('/will', function (req, res) {
